110e0eb1e858042ddbc4a7b03be7f821039dca93
mattiacoli <mattia.coli@aol.com>
2025-03-25 15:04:11 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+import codingLanguages from "./data/codingLanguage"
+
+function App() {
+
+
+  return (
+    <>
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/data/codingLanguage.js
@@ -0,0 +1,34 @@
+const codingLanguages = [
+  {
+    id: 1,
+    title: 'HTML',
+    content: 'HTML (HyperText Markup Language) è il linguaggio di markup standard per creare pagine web e applicazioni web. Fornisce la struttura di una pagina web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini È facile da imparare e ampiamente utilizzato per creare siti web statici.'
+  },
+  {
+    id: 2,
+    title: 'CSS',
+    content: 'CSS (Cascading Style Sheets) è un linguaggio di fogli di stile utilizzato per descrivere la presentazione di un documento scritto in HTML o XML. CSS descrive come gli elementi devono essere resi sullo schermo, sulla carta, nel discorso o su altri media. Permette di separare il contenuto dalla presentazione, migliorando la manutenibilità del codice.'
+  },
+  {
+    id: 3,
+    title: 'JavaScript',
+    content: 'JavaScript è un linguaggio di programmazione che è una delle tecnologie fondamentali del World Wide Web, insieme a HTML e CSS. Consente pagine web interattive ed è una parte essenziale delle applicazioni web. JavaScript può essere utilizzato sia sul lato client che sul lato server, rendendolo molto versatile.'
+  },
+  {
+    id: 4,
+    title: 'Node.js',
+    content: 'Node.js è un runtime JavaScript costruito sul motore JavaScript V8 di Chrome. Permette agli sviluppatori di utilizzare JavaScript per scrivere codice lato server e costruire applicazioni di rete scalabili. Node.js è noto per la sua efficienza e capacità di gestire molte connessioni simultanee.'
+  },
+  {
+    id: 5,
+    title: 'Express',
+    content: 'Express è un framework per applicazioni web minimalista e flessibile per Node.js che fornisce un set robusto di funzionalità per applicazioni web e mobili. È progettato per costruire applicazioni web e API. Express semplifica lo sviluppo di server web con Node.js, offrendo una serie di strumenti e middleware.'
+  },
+  {
+    id: 6,
+    title: 'ReactJS',
+    content: 'ReactJS è una libreria JavaScript per costruire interfacce utente. È mantenuta da Facebook e da una comunità di sviluppatori individuali e aziende. React può essere utilizzato come base nello sviluppo di applicazioni a pagina singola o mobili. È noto per la sua efficienza, flessibilità e facilità di integrazione con altre librerie o framework.'
+  }
+];
+
+export default codingLanguages;
