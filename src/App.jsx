ae4500089f7959b9f745b61aecfd3392a72808fa
mattiacoli <mattia.coli@aol.com>
2025-03-25 15:20:38 +0100

    add Header component and update App layout with navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import codingLanguages from "./data/codingLanguage"
+import Header from "./components/Header"
 
 function App() {
 
@@ -7,8 +8,31 @@ function App() {
   return (
     <>
 
+      <Header />
 
+      <div className="container mt-4">
+        <div class="card">
+          <div class="card-header">
 
+            <ul class="nav nav-pills card-header-pills gap-3">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">HTML</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">CSS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active">JavaScript</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="card-body">
+            <h4 class="card-title">HTML</h4>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+
+  return (
+    <header className="bg-light p-2">
+
+      <div className="mt-3">
+        <h1>Learn Web development</h1>
+      </div>
+
+    </header>
+  )
+}
